9a468c84e40b5aea3be591be12531b2546770ef4
ComputerSounds <44986572+ComputerSounds@users.noreply.github.com>
2023-06-24 13:18:04 -0400

    Refactor to use methods and rank by grade
    Grade parsing still shaky. Need to consolidate grade events somehow. Also need to deal with challenging up.
    May want to consider using scores.

--- scrapers/rating.py
+++ scrapers/rating.py
@@ -4,19 +4,56 @@ import re
 import operator
 import save_restore
 
-# Step 2: Initialize band ratings
-current_band_ratings = {}
 
-games_results_by_year = save_restore.import_from_json('test_dump')
+# ------------------------------------------------------
+# TODO: move to new class
+CURRENT_BAND_RATINGS = {}
 
-# Step 3: Iterate over the years in games_results_by_year
-for year in sorted(games_results_by_year.keys()):
-    year_results = games_results_by_year[year]
+def get_ratings(band_grade, band_name):
+    global CURRENT_BAND_RATINGS
 
-    # Step 4: Sort contests within the year based on dates
-    sorted_contests = sorted(year_results.values(), key=lambda x: x['date'])
+    if band_grade not in CURRENT_BAND_RATINGS:
+        CURRENT_BAND_RATINGS[band_grade] = {}
 
-    # Step 5: Iterate over contests in chronological order
+    if band_name not in CURRENT_BAND_RATINGS[band_grade]:
+        CURRENT_BAND_RATINGS[band_grade][band_name] = Rating()
+
+    rating = CURRENT_BAND_RATINGS[band_grade][band_name]
+    return rating
+
+
+def update_ratings(band_grade, band_name, rating):
+    global CURRENT_BAND_RATINGS
+    CURRENT_BAND_RATINGS[band_grade][band_name] = rating;
+
+def leaderboard():
+    global CURRENT_BAND_RATINGS
+    for grade_id in CURRENT_BAND_RATINGS.keys():
+        print("" + grade_id)
+        # print("------------")
+        leader_board_for_grade(grade_id)
+        print()
+
+def leader_board_for_grade(band_grade):
+    global CURRENT_BAND_RATINGS
+    # Print leaderboard of top N bands after each contest
+    N = 5  # Define the number of top bands to display
+
+    # Sort current_band_ratings based on the updated ratings in descending order
+    sorted_ratings = sorted(CURRENT_BAND_RATINGS[band_grade].items(), key=lambda x: ordinal(x[1]), reverse=True)
+
+    # Display the leaderboard of top N bands
+    for band_name, rating in sorted_ratings[:N]:
+        print(f"   {band_name: <40}: {ordinal(rating):.3f}")
+
+# ------------------------------------------------------
+
+def process_games(results_to_process):
+
+    # Sort contests within the year based on dates
+    sorted_contests = sorted(results_to_process.values(), key=lambda x: x['date'])
+
+    # Iterate over contests in chronological order
     for contest in sorted_contests:
 
         if not "results" in contest:
@@ -25,50 +62,60 @@ for year in sorted(games_results_by_year.keys()):
 
         contest_results = contest['results']
 
-        # Step 6: Iterate over divisions in contest results
-        for division in contest_results.keys():
-            actual_placings = contest_results[division]['placings']
+        # Iterate over grades in contest results
+        for grade_id in contest_results.keys():
+            actual_placings = contest_results[grade_id]['placings']
 
-            # Step 7: Prepare ratings and placings for OpenSkill rate function
-            ratings_of_competing_bands = []
-            ranks = []
-            for band_name, placing in actual_placings.items():
+            ratings_of_competing_bands, ranks, names_of_competing_bands = create_input_for_openskill(grade_id, actual_placings)
 
-                if placing == "DISQ":
-                    # Playing for sheets! Let's not draw conclusions....
-                    continue
-
-                # Strip off text like "EP" and "PP"
-                numeric_placing = re.search(r'\d+', placing).group()
-                ranks.append(int(numeric_placing))
-
-
-                # Initialize a new rating for the band if not already present
-                if band_name not in current_band_ratings:
-                    current_band_ratings[band_name] = Rating()
-                ratings_of_competing_bands.append( [current_band_ratings[band_name]] )
-
-
-
-            # Step 8: Update ratings using OpenSkill rate function with "Free-for-all" rule
+            # Compute updated ratings
             updated_band_ratings = rate(ratings_of_competing_bands, rank = ranks)
 
-            # Update current_band_ratings with the updated ratings
-            for i, band_name in enumerate(actual_placings.keys()):
-                # TODO - this doesn't match with the bands used in ranking.
-                # FIXME
-                current_band_ratings[band_name] = updated_band_ratings[i][0]
+            # Save updated ratings
+            for i, band_name in enumerate(names_of_competing_bands):
+                update_ratings(grade_id, band_name, updated_band_ratings[i][0])
 
-        # Print leaderboard of top N bands after each contest
-        N = 5  # Define the number of top bands to display
-
-        # Sort current_band_ratings based on the updated ratings in descending order
-        sorted_ratings = sorted(current_band_ratings.items(), key=lambda x: ordinal(x[1]), reverse=True)
-
-        # Display the leaderboard of top N bands
-        print(f"Leaderboard after contest: {contest['name']} on {contest['date']}")
-        for band_name, rating in sorted_ratings[:N]:
-            print(f"{band_name: <50}: {ordinal(rating):.3f}")
-
+        # Leaderboard
+        print(f"Leaderboard after {contest['name']} on {contest['date']}")
+        print("-----------------------------------------------")
+        leaderboard()
         print()
-# Final current_band_ratings contain the updated OpenSkill ratings for each band
+
+
+def create_input_for_openskill(grade, placings_table):
+
+    # Step 7: Prepare ratings and placings for OpenSkill rate function
+    names_of_competing_bands = []
+    ratings_of_competing_bands = []
+    ranks = []
+    for band_name, placing in placings_table.items():
+
+        if placing == "DISQ":
+            # Playing for sheets! Let's not draw conclusions....
+            continue
+
+        # Strip off text like "EP" and "PP"
+        numeric_placing = re.search(r'\d+', placing).group()
+        ranks.append(int(numeric_placing))
+
+        current_rating = get_ratings(grade, band_name)
+
+
+        ratings_of_competing_bands.append( [current_rating] )
+        names_of_competing_bands.append(band_name)
+
+    return ratings_of_competing_bands, ranks, names_of_competing_bands
+
+if __name__ == "__main__":
+
+    games_results_by_year = save_restore.import_from_json('test_dump')
+
+    for year in sorted(games_results_by_year.keys()):
+        year_results = games_results_by_year[year]
+
+        process_games(year_results)
+        #TODO FIXME REMOVE
+        break;
+
+
+
